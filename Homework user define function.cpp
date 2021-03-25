def72ee6a990344ba2cf2f20d2b05dc4e35b610c
sackpapee2558 <77197348+sackpapee2558@users.noreply.github.com>
2021-03-25 20:34:45 +0700

    Add files via upload

--- Homework user define function.cpp
+++ Homework user define function.cpp
@@ -1,30 +1,30 @@
 #include<iostream>
 using namespace std;
 float calAm(float am,float year);
-float am,year,totalam1,total4 = 0;
+float money,year,total,total4 = 0;
 int main()
 {
  cout<<"Khoayai BankInformation System**Interest = 3%**"<<endl;
  for(int i = 1;i<=3;i++)
  {
   cout<<"Input Amount "<<i<<": ";
-  cin>>am;
+  cin>>money;
   cout<<"Input Year "<<i<<": ";
   cin>>year;
-  cout<<"Total your money "<<i<<": "<<calAm(am,year)<<endl;
+  cout<<"Total your money "<<i<<": "<<calAm(money,year)<<endl;
   cout<<"*****************************************************"<<endl;
  }
 
- cout<<"Total 3 People = "<<totalam1<<endl;
+ cout<<"Total 3 People = "<<total<<endl;
  return(0);
 }
-float calAm(float am,float year)
+float calAm(float money,float year)
 {
- float totalam = (am*(year*0.03))+am;
- totalam1 += totalam;
+ float totalam = (money*(year*0.03))+money;
+ total += totalam;
  return(totalam);
  /*float total1 = year*0.03;
- float total2 = am*total1;
- float total3 = total2+am;
+ float total2 = money*total1;
+ float total3 = total2+money;
  return(total3);*/
 }
